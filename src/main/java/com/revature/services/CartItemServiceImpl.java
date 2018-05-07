ffd0efc968e1c76fe31c87c0d7529231a416cf61
Josh Dughi <jcd106@case.edu>
2018-05-07 12:06:37 -0400

    Added missing annotations

--- src/main/java/com/revature/services/CartItemServiceImpl.java
+++ src/main/java/com/revature/services/CartItemServiceImpl.java
@@ -2,11 +2,16 @@ package com.revature.services;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.revature.models.CartItem;
 import com.revature.repositories.CartItemRepository;
 
+@Service
+@Transactional
 public class CartItemServiceImpl implements CartItemService {
 
 	@Autowired
